ecf95594de2d75441a8712b775e0afbc5605e8c2
arunavan <arunajava567@gmail.com>
2024-06-04 18:01:01 +0530

    Add files via upload

---
+++ threads/SynchronizedDemo.java
@@ -0,0 +1,41 @@
+package com.ey.threads;
+
+class Display //target
+{ public  void wish(String name)
+ {  
+	synchronized(this) {
+	for(int i=0;i<10;i++)
+   {   System.out.print("good morning  ");
+    	try
+    	{ Thread.sleep(1000);
+    	}catch(InterruptedException ie)
+    	{}
+    System.out.println(name);
+   }//for
+	}
+	
+ }//wish
+}
+class MyThread extends Thread
+{ Display d;
+  String name;
+  MyThread(Display d, String name)
+  {   this.d=d;
+     this.name=name;
+  }
+  public void run()
+  {   d.wish(name);
+  }
+}
+
+class SynchronizedDemo
+{public static void main(String args[])
+  {Display d= new Display();  //sharedobject
+  MyThread t1= new MyThread(d,"Raj");
+  MyThread t2= new MyThread(d,"Ravi");
+   t1.start();
+   t2.start();
+
+   }
+
+}
