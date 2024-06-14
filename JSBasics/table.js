161e0ddce3615b019c527305e48e291245f23acf
arunavan <arunajava567@gmail.com>
2024-06-14 09:50:06 +0530

    Add files via upload

---
+++ JSBasics/table.js
@@ -0,0 +1,14 @@
+
+
+function table(){
+  for(x=1;x<=10;x++) {
+   document.writeln("<br>"+ 5 +" X"+ x +"="+5*x);
+ }
+  var y=1;
+  while(y<=10) {
+   document.writeln("<br>"+ 5 +" X"+ y +"="+5*y);
+   y++;
+ }
+
+
+}
