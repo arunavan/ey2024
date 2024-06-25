5d9ebcdcb4fb29880792326184c8d1735257da4c
arunavan <arunajava567@gmail.com>
2024-06-25 10:41:00 +0530

    Add files via upload

---
+++ react/App.js
@@ -0,0 +1,73 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+
+
+/*
+function App() {
+  return (
+    <div className="App">
+      
+     <h1>single Page App </h1>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+
+/*  Class Component
+
+import React,{ Component } from "react";
+class App  extends Component{
+  render(){
+    return ( 
+     <div>
+          <h1> My forst React class Component </h1>
+      </div>
+    );
+  }
+}
+export default App;
+
+*/
+// funcitonal component
+
+
+/*
+function App(){
+return (
+<div>
+      <h1> My first react functional compoennt</h1>
+</div>
+);
+}
+ export default App;
+*/
+
+//arrow function 
+export default App => {
+  return (
+    <div>
+          <h1> My first react functional compoennt</h1>
+    </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
