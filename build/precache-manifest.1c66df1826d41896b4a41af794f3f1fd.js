110429a5fe69ab2b56c9b429633696e00d54fe9b
arunavan <arunajava567@gmail.com>
2024-07-11 12:37:35 +0530

    Add files via upload

---
+++ build/precache-manifest.1c66df1826d41896b4a41af794f3f1fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f25900e82aa6c5ad0f4d",
+    "url": "/static/css/main.01813705.chunk.css"
+  },
+  {
+    "revision": "f25900e82aa6c5ad0f4d",
+    "url": "/static/js/main.20878ae3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1868546b76755d13629b",
+    "url": "/static/js/2.cdefec29.chunk.js"
+  },
+  {
+    "revision": "2ad3a9105827ccd10e0b59c1ee6cf8a1",
+    "url": "/index.html"
+  }
+];
